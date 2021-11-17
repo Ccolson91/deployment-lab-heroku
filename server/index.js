ea0d464db2736a7b9cfd3b30d8f5b0a62e2b4cea
Christian Colson <christiancolson91@gmail.com>
2021-11-17 14:53:46 -0600

    js and css endpoints added

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+app.use('/css', express.static(path.join(__dirname, 'public/style.css')))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.get('/js', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/main.js'))
+})
+
+app.get('/css', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/style.css'))
 })
 
 
